3833193439fb8395251044dc4a6f9e68059250a4
BaraiyaKuldip <baraiyakuldip302@gmail.com>
2023-10-17 19:20:58 +0530

    string

---
+++ ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int size;
+printf("Enter Your Name Size : ");
+scanf("%d",&size);
+char name[size];
+printf("\n Enter Your Name : ");
+scanf("%s",&name);
+printf("Your Name : %s",name);
+
+
+
+
+}
