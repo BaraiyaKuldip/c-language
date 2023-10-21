6d16f0bb80737cf52035908fd23f26f414c4f533
BaraiyaKuldip <baraiyakuldip302@gmail.com>
2023-10-21 18:57:52 +0530

    ex3

--- ex3.c
+++ ex3.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-void main()
-{
-    int i;
+int main()  
+{  
+    char sing_ch;  
 
-    char latter[5];
-
-    printf("Enter Your Input : ");
-    scanf("%s", latter);
-
-    printf("Entered Input : %s ", latter);
-
-
+    printf("Input a character: ");  
+    scanf("%c", &sing_ch);  
+  
+    /* Checks whether it is an alphabet */  
+    if((sing_ch>='a' && sing_ch<='z') || (sing_ch>='A' && sing_ch<='Z'))  
+    {  
+        printf("This is an alphabet.\n");  
+    }  
+    else if(sing_ch>='0' && sing_ch<='9') /* whether it is digit */  
+    {  
+        printf("This is a digit.\n");  
+    }  
+    else /* Else special character */  
+    {  
+        printf("This is a special character.\n");  
+    }  
 }
